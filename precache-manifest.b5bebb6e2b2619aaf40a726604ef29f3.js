2dd608d5217bf1740780a6b67c82a52816ad0e8e
Alisha.bashir <bussinessmail1257@gmail.com>
2020-05-03 13:49:09 -0700

    Updates

---
+++ precache-manifest.b5bebb6e2b2619aaf40a726604ef29f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08aec4fa2558fd1f9c79979614f4778",
+    "url": "/todo-routine-app/index.html"
+  },
+  {
+    "revision": "45f4189006b0f1acf92e",
+    "url": "/todo-routine-app/static/css/main.fccc54dc.chunk.css"
+  },
+  {
+    "revision": "9f2302c323bc3b8cdb7d",
+    "url": "/todo-routine-app/static/js/2.2fbe07c3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-routine-app/static/js/2.2fbe07c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f4189006b0f1acf92e",
+    "url": "/todo-routine-app/static/js/main.fb9d268b.chunk.js"
+  },
+  {
+    "revision": "746a63d932584761f98c",
+    "url": "/todo-routine-app/static/js/runtime-main.0c475621.js"
+  }
+]);
